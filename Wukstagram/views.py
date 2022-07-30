8f1a4db471ee491e24b6df346fb0598fcb10ae33
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-30 17:50:13 +0900

    PyCharm django 인스타그램 프로젝트

---
+++ Wukstagram/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class Sub(APIView):
+  def get(self, request):
+    print("GET으로 호출")
+    return render(request, "wukstagram/main.html")
+
+  def post(self, request):
+    print("POST으로 호출")
+    return render(request, "wukstagram/main.html")
